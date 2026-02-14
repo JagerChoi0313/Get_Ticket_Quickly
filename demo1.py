972fca7313f54aa3030f76233ebb2de2929d5151
JagerChoi <17322905680@qq.com>
2026-02-14 23:30:15 +0800

    Update demo1.py - add click '不，选座购票' button functionality

--- demo1.py
+++ demo1.py
@@ -10,7 +10,7 @@ damai_url="https://www.damai.cn/"
 login_url="https://passport.damai.cn/login?ru=https%3A%2F%2Fwww.damai.cn%2F"
 
 #抢票页面
-target_url="https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.723c4d15lc4cXL&id=1014297695248&clicktitle=2026%E4%BA%94%E7%99%BE%E9%87%8C%E9%9F%B3%E4%B9%90%E8%8A%82"
+target_url="https://detail.damai.cn/item.htm?spm=a2oeg.home.card_0.ditem_1.275523e1kiDZzj&id=1018231634918"
 
 
 class Concert:
@@ -37,10 +37,20 @@ class Concert:
     def get_cookie(self):
         cookies = pickle.load(open('cookies.pkl','rb'))
         for cookie in cookies:
-            print(cookie)
-            self.driver.add_cookie(cookie)
+            # 使用原始的cookie信息，只移除可能导致问题的字段
+            cookie_dict = cookie.copy()
+            if 'expiry' in cookie_dict:
+                del cookie_dict['expiry']
+            # 确保domain正确
+            if 'domain' in cookie_dict and cookie_dict['domain']:
+                pass  # 使用原始domain
+            else:
+                cookie_dict['domain'] = '.damai.cn'
+            self.driver.add_cookie(cookie_dict)
+        print("###载入cookie成功###")
 
-        """登录"""
+
+    """登录"""
     def login(self):
         #如果为0就模拟登录一下
         if self.login_method == 0:
@@ -51,10 +61,68 @@ class Concert:
                 # 登录一下 登录信息记录
                 self.set_cookies()
             else:
-                self.driver.get(target_url)
+                # 先访问大麦网首页，确保域名匹配
+                self.driver.get(damai_url)
                 # 登录一下 通过selenium传入用户信息
                 self.get_cookie()
+                # 再访问目标页面
+                self.driver.get(target_url)
+
+    """打开浏览器"""
+    def enter_concert(self):
+        print("###打开浏览器，进入大麦网###")
+        # 调用login
+        self.login()
+        self.driver.refresh()
+        self.status=2
+        print("###登录成功###")
+
+
+    # 抢票并且下单
+    def choose_ticket(self):
+        if self.status == 2:
+            print('='*30)
+            print("###请开始日期选择和票价选择###")
+            while self.driver.title.find("确认订单")==1:
+                # 下单按钮
+                buybutton=self.driver.find_element_by_class_name("buybtn").text
+                if buybutton == '提交缺货登记':
+                    self.driver.refresh()
+                elif buybutton == '立即购票':
+                    self.driver.find_element_by_class_name("buybtn").click()
+                elif buybutton == '选座购买':
+                    self.driver.find_element_by_class_name("buybtn").click()
+                    self.status =4
+                else:
+                    self.status =100
+                title=self.driver.title
+                if title=='选座购买':
+                    # 执行选座位操作
+                    self.status =10
+                elif title=='确认订单':
+                    # 实现下单逻辑
+                    while True:
+                        print('###正在加载###')
+                        self.check_order()
+                        break
+
+    """下单操作"""
+    def check_order(self):
+        print("###开始确认订单###")
+        try:
+            self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div[2]/svg').click()
+        except Exception as e:
+            print("###购票人信息选中失败，自行查看元素位置###")
+            print(e)
+        time.sleep(0.5)
+        self.driver.find_element_by_xpath('//*[@id="root"]/div/div[14]/div/div[2]/button').click()
+
+
 
 if __name__ == '__main__':
    con=Concert()
-   con.login()
+   con.enter_concert()
+   con.choose_ticket()
+
+   # 保持浏览器窗口打开
+   input("###抢票程序已启动，按回车键退出###")
